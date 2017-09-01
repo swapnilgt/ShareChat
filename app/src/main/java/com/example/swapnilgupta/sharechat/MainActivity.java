6385055a253886d99b0e9386c6dcd9e63d110f95
swapnilgt <swapnilgupta.iiith@gmail.com>
2017-09-01 21:11:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/swapnilgupta/sharechat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.swapnilgupta.sharechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
